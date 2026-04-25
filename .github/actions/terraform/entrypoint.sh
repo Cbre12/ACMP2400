421369b04297381d06580435301771ae1689270a
Cole Breinig <167512984+Cbre12@users.noreply.github.com>
2026-04-25 04:55:22 -0500

    Refactor Terraform commands to use working directory

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -9,5 +9,6 @@ export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
 export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
 
-terraform plan
-terraform apply --auto-approve
+terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
+terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
